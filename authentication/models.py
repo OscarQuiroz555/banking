3afca6a44b82edd1c3bf3b616422b885ccd1ca83
Anderson <andavidtr@gmail.com>
2025-09-10 09:09:38 -0500

    modelo relacional arreglado

--- authentication/models.py
+++ authentication/models.py
@@ -2,18 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class User(models.Model):
-    firstname = models.CharField(max_length=20)
-    lastname  = models.CharField(max_length=20, blank=True)
-    mobile_phone = models.CharField(max_length=20)
-    address = models.CharField(max_length=100, blank=True)
-    email = models.EmailField(max_length=50)
-    password = models.TextField()
-    #id_city = models.ForeignKey("City", on_delete=models.SET_NULL, null=True) #error
-    status = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(null=True, blank=True)
 
 class Country(models.Model):
     name = models.CharField(max_length=30)
@@ -35,3 +23,16 @@ class City(models.Model):
     abrev = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class User(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname  = models.CharField(max_length=20, blank=True)
+    mobile_phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100, blank=True)
+    email = models.EmailField(max_length=50)
+    password = models.TextField()
+    id_city = models.ForeignKey(City, on_delete=models.CASCADE) #error
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
